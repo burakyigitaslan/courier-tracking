b8642a41069d1f75fb896330ac8ee83414e7cce0
Burak Yiğitaslan <yigitaslan.burak@gmail.com>
2026-02-22 13:53:30 +0300

    add dockerfile and docker compose with postgresql, rabbitmq, and pgadmin services

---
+++ run.cmd
@@ -0,0 +1,3 @@
+@echo off
+echo Starting Application Stack with Docker...
+docker compose up --build
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Application Stack with Docker..."
+docker compose up --build
